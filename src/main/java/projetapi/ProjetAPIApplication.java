468fdc507b8d9b1356abc9dcf1de85da8a747f51
Anaïs Thiriot <anaisthiriot@hotmail.fr>
2019-02-07 16:28:45 +0100

    Copie du projet Tache

---
+++ src/main/java/projetapi/ProjetAPIApplication.java
@@ -0,0 +1,34 @@
+package projetapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class ProjetAPIApplication {
+
+	
+	public static void main(String[] args) {
+        SpringApplication.run(ProjetAPIApplication.class, args);
+    }
+
+	@Bean
+    public Docket swaggerProjetAPI() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("application"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(new ApiInfoBuilder().version("1.0")
+                        .title("PROJET API")
+                        .description("Documentation PROJET API")
+                        .build());
+    }
+}
